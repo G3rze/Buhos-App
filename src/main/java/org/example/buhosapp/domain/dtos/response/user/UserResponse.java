444178c9c219a4befdbf15cbc3e1930da0a8f493
Elias8420 <eliaszelaya747@gmail.com>
2026-06-20 04:34:45 -0600

    fix: error to build a response because the dto don't have builder  annotattion

--- src/main/java/org/example/buhosapp/domain/dtos/response/user/UserResponse.java
+++ src/main/java/org/example/buhosapp/domain/dtos/response/user/UserResponse.java
@@ -1,5 +1,10 @@
 package org.example.buhosapp.domain.dtos.response.user;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class UserResponse {
     private String username;
     private String card;
