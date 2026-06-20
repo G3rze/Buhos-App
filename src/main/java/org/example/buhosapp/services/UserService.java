224d8676fc731bf5a5188c739e0a84a2c4e19a1b
Elias8420 <eliaszelaya747@gmail.com>
2026-06-20 17:20:14 -0600

    refactor: service for create user

--- src/main/java/org/example/buhosapp/services/UserService.java
+++ src/main/java/org/example/buhosapp/services/UserService.java
@@ -3,6 +3,8 @@ package org.example.buhosapp.services;
 import org.example.buhosapp.domain.dtos.request.user.CreateUserRequest;
 import org.example.buhosapp.domain.dtos.response.user.UserResponse;
 
+import java.util.UUID;
+
 public interface UserService {
-    UserResponse createUser(CreateUserRequest userRequest);
+    UserResponse createUser(CreateUserRequest userRequest, String role);
 }
